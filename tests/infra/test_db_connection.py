471e8949033015ad33d592c94e1135351feb36c7
Walber Vaz da Silva <wvs.walber@gmail.com>
2024-02-13 12:33:32 -0300

    test: skip test connection db

--- tests/infra/test_db_connection.py
+++ tests/infra/test_db_connection.py
@@ -1,6 +1,9 @@
+from pytest import mark
+
 from src.infra.db.settings.connection import DBConnectionHandler
 
 
+@mark.skip(reason="Sensitive data")
 def test_create_database_engine():
     engine = DBConnectionHandler().get_engine()
 
