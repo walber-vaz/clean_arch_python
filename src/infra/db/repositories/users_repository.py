2d523504810375c6a4e5cf83f1aa5cd56fb838d8
Walber Vaz da Silva <wvs.walber@gmail.com>
2024-02-16 08:53:05 -0300

    test: add select method to UsersRepository and test

--- src/infra/db/repositories/users_repository.py
+++ src/infra/db/repositories/users_repository.py
@@ -1,3 +1,5 @@
+from typing import Any
+
 from src.infra.db.entities.users import Users as UsersEntity
 from src.infra.db.settings.connection import DBConnectionHandler
 
@@ -16,6 +18,23 @@ class UsersRepository:
                     db_connection.session.refresh(new_user)
                 else:
                     raise Exception("DB connection session is None.")
+            except Exception as exception:
+                if db_connection.session is not None:
+                    db_connection.session.rollback()
+                raise exception
+
+    @classmethod
+    def select(cls, id: int) -> Any:
+        with DBConnectionHandler() as db_connection:
+            try:
+                if db_connection.session is None:
+                    raise Exception("DB connection session is None.")
+                user = (
+                    db_connection.session.query(UsersEntity)
+                    .filter(UsersEntity.id == id)
+                    .first()
+                )
+                return user
             except Exception as exception:
                 if db_connection.session is not None:
                     db_connection.session.rollback()
--- tests/infra/db/test_users_repository.py
+++ tests/infra/db/test_users_repository.py
@@ -1,8 +1,14 @@
+from pytest import mark
+from sqlalchemy import text
+
 from src.infra.db.repositories.users_repository import UsersRepository
+from src.infra.db.settings.connection import DBConnectionHandler
 
-# from src.infra.db.settings.connection import DBConnectionHandler
+db_connection = DBConnectionHandler()
+connection = db_connection.get_engine().connect()
 
 
+@mark.skip(reason="Sensitive data")
 def test_insert_user():
     mocked_user = {
         "name": "Any Name",
@@ -17,3 +23,73 @@ def test_insert_user():
         email=str(mocked_user.get("email")),
         role=str(mocked_user.get("role")),
     )
+
+    sql = """
+        SELECT id, name, email, role FROM users
+        WHERE name = "{}"
+        AND email = "{}"
+        AND role = "{}";
+    """.format(
+        mocked_user.get("name"),
+        mocked_user.get("email"),
+        mocked_user.get("role"),
+    )
+
+    response = connection.execute(text(sql))
+    registry = response.fetchall()[0]
+
+    assert registry.name == mocked_user.get("name")
+    assert registry.email == mocked_user.get("email")
+    assert registry.role == mocked_user.get("role")
+
+    connection.execute(
+        text(
+            f"""
+                DELETE FROM users
+                WHERE id = {registry.id};
+            """
+        )
+    )
+    connection.commit()
+
+
+@mark.skip(reason="Sensitive data")
+def test_select_user():
+    mocked_user = {
+        "name": "Any Name 2",
+        "password": "password",
+        "email": "any2@email.com",
+        "role": "user",
+    }
+
+    sql = f"""
+        INSERT INTO users (name, password, email, role)
+        VALUES (
+            "{mocked_user.get("name")}",
+            "{mocked_user.get("password")}",
+            "{mocked_user.get("email")}",
+            "{mocked_user.get("role")}"
+        );
+    """
+
+    res = connection.execute(text(sql))
+    connection.commit()
+    registry = res.lastrowid
+
+    user_repository = UsersRepository()
+    query = user_repository.select(registry)
+
+    assert query.id == registry
+    assert query.name == mocked_user.get("name")
+    assert query.email == mocked_user.get("email")
+    assert query.role == mocked_user.get("role")
+
+    connection.execute(
+        text(
+            f"""
+                DELETE FROM users
+                WHERE id = {registry};
+            """
+        )
+    )
+    connection.commit()
